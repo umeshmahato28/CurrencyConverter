83c1db7bdeae17a2e0ca985cd05a1dd23b7a3b67
umeshmahato28 <mahatoumesh94@gmail.com>
2024-01-22 12:42:07 +0530

    bg changes

--- src/App.jsx
+++ src/App.jsx
@@ -31,11 +31,11 @@ function App() {
 
   return (
     <>
-      <div className='bg-green-200 flex h-screen items-center'>
-        <div className='w-[29rem] h-[29rem] p-5 m-auto rounded-md shadow-2xl shadow-emerald-800/75 bg-blue-200'>
-          <h2 className='p-4 mt-9 m-4 mb-8 text-center font-bold rounded-xl shadow-lg shadow-red-400/75 bg-red-300'>CURRENCY CONVERTER </h2>
+      <div className='flex items-center h-screen max-w-screen-3xl bg-center bg-cover bg-[url("https://images.pexels.com/photos/4968396/pexels-photo-4968396.jpeg")] '>
+        <div className='w-[29rem] h-[29rem] p-5 m-auto rounded-lg shadow-2xl shadow-slate-100/75 bg-slate-950/60'>
+          <h2 className='p-4 mt-9 m-4 mb-8 text-center font-bold text-2xl rounded-xl shadow-lg  bg-slate-400'>CURRENCY CONVERTER </h2>
           <div className='m-4 space-y-3'>
-            <span className='p-2 text-green-600 font-semibold' text=""> From </span>
+            <span className='p-2 text-slate-100 font-semibold' text=""> From </span>
             <select className='font-medium rounded-sm shadow-md' value={curr} onChange={(e) => { setCurr(e.target.value) }}>
               {currencyData.map((result) => (
                 <option key={result.key} value={result.key}>
@@ -44,11 +44,11 @@ function App() {
               ))}
             </select>
             <br />
-            <input className='p-2 w-full items-center border border-blue-500 rounded-md' type="number" min='0' onChange={(e) => { setFromData(e.target.value) }} /><br />
+            <input className='p-2 w-full items-center bg-slate-300 border border-blue-500 rounded-md' type="number" min='0' onChange={(e) => { setFromData(e.target.value) }} /><br />
           </div>
 
           <div className='m-4 space-y-3'>
-            <label className='p-2 text-green-600 font-semibold' htmlFor="">To </label>
+            <label className='p-2 text-slate-100 font-semibold' htmlFor="">To </label>
             <select className='font-medium rounded-sm shadow-md' value={tocurr} onChange={(e) => { setToCurr(e.target.value) }}>
               {currencyData.map((result) => (
                 <option key={result.key} value={result.key}>
@@ -57,7 +57,7 @@ function App() {
               ))}
             </select>
             <br />
-            <input className='p-2 w-full items-center border border-blue-500 rounded-md' type="number" value={todata} id="amount" readOnly /><br />
+            <input className='p-2 w-full items-center bg-slate-300 border border-blue-500 rounded-md' type="number" value={todata} id="amount" readOnly /><br />
           </div>
           <button className="ml-44 bg-blue-300 shadow-lg shadow-blue-500/75 rounded-lg p-2 m-3" onClick={convert}>
             CONVERT
